0d67a644a297ef1e83204a21d4ed9ebf244b93c5
MeD-Owner <vt888@hotmail.com>
2021-02-25 18:31:06 -0500

    Update Adapt-accessibilityfixes.js

--- assets/Adapt-accessibilityfixes.js
+++ assets/Adapt-accessibilityfixes.js
@@ -1,13 +1,19 @@
 // ================================================
 //
 //		Adapt ESDC Accessibility fixes script
-//		v.1.1.1
-//		2021-02-25
 //
 // ================================================
 
 
+
+// -------------------------------------------------------------------------
+//
+//		Update listeners - functions which decide when to run fixes
+//
+// -------------------------------------------------------------------------
+
 //Startup update
+//-----------------------------------------------------------------------------
 window.setTimeout(function(){
 
 }, 500);
@@ -16,11 +22,10 @@ window.setTimeout(function(){
 //event listener for changes in Adapt page. It checks the "data-location" 
 //attribute of the html tag for updates.
 //-----------------------------------------------------------------------------
-
 var observer = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
 	if (mutation.type == "attributes" && mutation.attributeName == "data-location") {
-		setTimeout(pagefix, 200);
+		setTimeout(accessibilityfixes, 200);
 		}
 	});
 });
@@ -28,53 +33,125 @@ var observer = new MutationObserver(function(mutations) {
 //Note: document.documentElement is a pointer to the <html> node
 //it can be replaced with a selector like document.getElementById("id") 
 //to target other tags.
+//-----------------------------------------------------------------------------
 observer.observe(document.documentElement, {
 	attributes: true
 });
 
-// runs on navigation
-function pagefix (){
-// write code that applies everywhere here:
+//This function runs anytime anything in the DOM (inside #wrapper) is modified
+//DON'T DO IT !
+//-----------------------------------------------------------------------------
+$('#wrapper').on('DOMSubtreeModified', function(){
 
 
-	// On menu
-	if($('#adapt').attr('data-location') == 'course'){
+});
+
+
+
+
+
+// -------------------------------------------------------------------------
+//
+//		Accessibility fixes - grouped by global fixes, fixes for the menu 
+//      only and fixes for all other pages of the course
+//
+// -------------------------------------------------------------------------
+
+function accessibilityfixes (){
+	
+	// -------------
+	// GLOBAL FIXES
+	// -------------
+
+	//Tab order of buttons in the top navigation bar
+	var buttonArray = [];
+	$(".navigation-inner button").each(function(i){
+		buttonArray.push([i, $(this).position().left]);		
+	});
+	buttonArray.sort(sortmulti(1, comparator, false));
+	for (var j=0; j < buttonArray.length; j++){
+	$(".navigation-inner button").eq(buttonArray[j][0]).attr("tabindex", j+1);
+
+	//target blank to external sites (ignore local hrefs)
+	//-----------------------------------------------------------------------------
+	$( 'a' ).each(function() {
+  		if( location.hostname === this.hostname || !this.hostname.length ) {
+  		} else {
+     		$(this).attr('target', '_blank');
+  		}
+	});
+
+	//Standard ARIA label conversions and control of aria hidden attributes
+	//-----------------------------------------------------------------------------	
+	$('img').each(function(){
 		
-		$('img').each(function(){
-	  		if ($(this).attr('aria-hidden') == 'true'){
-	    	$(this).removeAttr('aria-hidden');
-	    	$(this).attr('alt', '');
-      		}
-      	});
+		// if image has already has alt
+		if ($(this).hasAttr('alt')){
+			//do nothing
+		}
+		
+		//if img has aria-label, convert to alt
+		else if ($(this).hasAttr('aria-label')){
+			$(this).attr("alt", $(this).attr("aria-label"));
+			$(this).removeAttr("aria-label");		
+		}
+		
+		//image had neither aria-label or alt, add an empty alt
+		else{
+			
+			$(this).attr('alt', '');
+		}
+		
+		// if image has aria-hidden, remove it.
+		if ($(this).attr('aria-hidden') == 'true'){
+			$(this).removeAttr('aria-hidden');
+		}
+		
+	});
+	
+	// ----------------
+	// 
+	// ----------------	
 
-	    var buttonArray = [];
-		$(".navigation-inner button").each(function(i){
-			buttonArray.push([i, $(this).position().left]);		
-		});
-		buttonArray.sort(sortmulti(1, comparator, false));
-		for (var j=0; j < buttonArray.length; j++){
-		$(".navigation-inner button").eq(buttonArray[j][0]).attr("tabindex", j+1);
+
+	
+	if($('#adapt').attr('data-location') == 'course'){
+	// ----------------
+	// MENU FIXES
+	// ----------------
+
+
+
+		
+	// ----------------
+	// 
+	// ----------------	
 	}
 
-	// in a page
+
+
 	}else {
+	// ------------------
+	// COURSE PAGE FIXES
+	// ------------------
 
 		// multiple choice label fix
+		//-----------------------------------------------------------------------------
 		let multiChoiceComponents = $(".mcq-component");
 		multiChoiceComponents.each(function(i){
-		let label = $(this).attr("data-adapt-id") + "qlabel";
-		alert($(this));
-		$(".mcq-body-inner > p").attr("id", label);
-		$(".mcq-widget").attr("aria-labelledby", label);
+			let label = $(this).attr("data-adapt-id") + "qlabel";
+			$(".mcq-body-inner > p").attr("id", label);
+			$(".mcq-widget").attr("aria-labelledby", label);
 		});
 
 		//Matching questions fix
-
+		//-----------------------------------------------------------------------------
 		$(".matching-select-container").each(function(k){
 			var glabel = $(this).parents().find('.matching-component').attr("data-adapt-id")+'_qlabel_'+k;
 			$(this).find(".dropdown__inner").attr('id', glabel);
 			$(this).find("button").attr('aria-labelledby', glabel);
 		});
+		
 		//Accordion component accessibility fixes
 		//-----------------------------------------------------------------------------
 		$(".accordion-component").each(function(){
@@ -89,53 +166,24 @@ function pagefix (){
 		//remove tooltips from buttons
 		//-----------------------------------------------------------------------------
 		$("button").removeAttr("tooltip");
-
-		//target blank to external sites (ignore local hrefs)
-		//-----------------------------------------------------------------------------
-		$( 'a' ).each(function() {
-  			if( location.hostname === this.hostname || !this.hostname.length ) {
-  			} else {
-      		$(this).attr('target', '_blank');
-  			}
-		});
-
-		//Standard ARIA label conversions and control of aria hidden attributes
-		//-----------------------------------------------------------------------------	
-		$('img').each(function(){
-			// if image has aria-hidden, get alt empty instead
-			if ($(this).attr('aria-hidden') == 'true'){
-			$(this).removeAttr('aria-hidden');
-			$(this).attr('alt', '');
-			}
-			// if image has aria-label, convert to alt
-			if($(this).attr('aria-label') == ''){
-			$(this).removeAttr('aria-label');
-			$(this).attr('alt', '');
-			}else {
-			$(this).attr("alt", $(this).attr("aria-label"));
-			$(this).removeAttr("aria-label");
-			}
-			// if image has no aria-hidden and alt
-			var attr = $(this).attr('alt');
-			var attraria = $(this).attr('aria-label');
-			if (typeof attr !== typeof undefined && attr !== false){}else {
-			if (typeof attraria !== typeof undefined && attraria !== false){}else{$(this).attr('alt', '');}}
-		});
+		
+		
+	// ----------------
+	// 
+	// ----------------	
 	}
+	
 }
 
 
 
 
-//Update recursively (don't do it)
-$('#wrapper').on('DOMSubtreeModified', function(){
 
-
-});
-
-
-//utility functions
-//=============================================================
+// -------------------------------------------------------------------------
+//
+//		Utility functions
+//
+// -------------------------------------------------------------------------
 
 //sort a multidimensional array
 //-----------------------------------------------------------------------------
@@ -153,3 +201,13 @@ function sortmulti(n, comparatorFunction, reverse) {
         }
     }
 }
+
+//Check if object has given attribute
+Object.prototype.hasAttr = function(attr) {
+    if(this.attr) {
+        var _attr = this.attr(attr);
+    } else {
+        var _attr = this.getAttribute(attr);
+    }
+    return (typeof _attr !== "undefined" && _attr !== false && _attr !== null);      
+};
