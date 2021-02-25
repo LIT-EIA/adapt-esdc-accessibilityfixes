5bffcca7674eb632a55ca0b7979c28e83848d7d9
Timbro <vincent.timbro@servicecanada.gc.ca>
2021-02-25 18:29:17 -0500

    v0.0.3?

--- assets/Adapt-accessibilityfixes.js
+++ assets/Adapt-accessibilityfixes.js
@@ -1,24 +1,31 @@
 // ================================================
 //
 //		Adapt ESDC Accessibility fixes script
-//		v.1.1.0
-//		2021-01-22
 //
 // ================================================
+
+
+
+// -------------------------------------------------------------------------
+//
+//		Update listeners - functions which decide when to run fixes
+//
+// -------------------------------------------------------------------------
+
+//Startup update
+//-----------------------------------------------------------------------------
+window.setTimeout(function(){
+
+}, 500);
+
 
 //event listener for changes in Adapt page. It checks the "data-location" 
 //attribute of the html tag for updates.
 //-----------------------------------------------------------------------------
-
 var observer = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
-		if (mutation.type == "attributes" && mutation.attributeName == "data-location") {
-			accessibilityfixes();
-			setTimeout(accessibilityfixes, 500);
-			setTimeout(accessibilityfixes, 1000);
-			setTimeout(accessibilityfixes, 1500);
-			setTimeout(accessibilityfixes, 3000);
-			setTimeout(accessibilityfixes, 10000);
+	if (mutation.type == "attributes" && mutation.attributeName == "data-location") {
+		setTimeout(accessibilityfixes, 200);
 		}
 	});
 });
@@ -26,90 +33,157 @@ var observer = new MutationObserver(function(mutations) {
 //Note: document.documentElement is a pointer to the <html> node
 //it can be replaced with a selector like document.getElementById("id") 
 //to target other tags.
+//-----------------------------------------------------------------------------
 observer.observe(document.documentElement, {
 	attributes: true
 });
 
-
-//run accessibility script once on page load
-$(document).ready(function(){
-	accessibilityfixes();
-});
-
-//also run it on: all DOM loaded
-document.addEventListener("DOMContentLoaded", function(){
-    accessibilityfixes();
-});
-
-//also run it on: page fully loaded
-window.addEventListener("load", function(){
-    accessibilityfixes();
-});
-
-
-//function to fix accessibility issues
+//This function runs anytime anything in the DOM (inside #wrapper) is modified
+//DON'T DO IT !
 //-----------------------------------------------------------------------------
-function accessibilityfixes()
-{
-	//Not an accessibility fix, just nice to have
-	//Force all links to open in a new window / tab
-	//-------------------------------------------------------------------------
-	$("a").prop("target", "_blank");
+$('#wrapper').on('DOMSubtreeModified', function(){
+
+
+});
+
+
+
+
+
+// -------------------------------------------------------------------------
+//
+//		Accessibility fixes - grouped by global fixes, fixes for the menu 
+//      only and fixes for all other pages of the course
+//
+// -------------------------------------------------------------------------
+
+function accessibilityfixes (){
 	
-	//change all aria-labels on images to Alt tags
-	//-----------------------------------------------------------------------------
-	$("img").each(function(){
-		$(this).attr("alt", $(this).attr("aria-label"));
-		$(this).removeAttr("aria-label");
-	});
-	
-	//remove tooltips from buttons
-	//-----------------------------------------------------------------------------
-	$("button").removeAttr("tooltip");
-	
-	//fix for heading tab orders
-	//-----------------------------------------------------------------------------
+	// -------------
+	// GLOBAL FIXES
+	// -------------
+
+	//Tab order of buttons in the top navigation bar
 	var buttonArray = [];
 	$(".navigation-inner button").each(function(i){
 		buttonArray.push([i, $(this).position().left]);		
 	});
 	buttonArray.sort(sortmulti(1, comparator, false));
 	for (var j=0; j < buttonArray.length; j++){
-		$(".navigation-inner button").eq(buttonArray[j][0]).attr("tabindex", j+1);
-	}
-	
-	//General fixes for all question types
+	$(".navigation-inner button").eq(buttonArray[j][0]).attr("tabindex", j+1);
+
+	//target blank to external sites (ignore local hrefs)
 	//-----------------------------------------------------------------------------
-	
-	//Narrative component accessibility fixes
-	//-----------------------------------------------------------------------------
-	
-	//Accordion component accessibility fixes
-	//-----------------------------------------------------------------------------
-	
-	$(".accordion-component").each(function(){
-		var parentID = $(this).attr("data-adapt-id");
-		$(".accordion-item-title").each(function(i){
-			var blockid = "accord-" + i + "-" + parentID;
-			$(this).attr("aria-controls", blockid);
-			$(this).next().attr('id', blockid);
-		});
+	$( 'a' ).each(function() {
+  		if( location.hostname === this.hostname || !this.hostname.length ) {
+  		} else {
+     		$(this).attr('target', '_blank');
+  		}
+	});
+
+	//Standard ARIA label conversions and control of aria hidden attributes
+	//-----------------------------------------------------------------------------	
+	$('img').each(function(){
+		
+		// if image has already has alt
+		if ($(this).hasAttr('alt')){
+			//do nothing
+		}
+		
+		//if img has aria-label, convert to alt
+		else if ($(this).hasAttr('aria-label')){
+			$(this).attr("alt", $(this).attr("aria-label"));
+			$(this).removeAttr("aria-label");		
+		}
+		
+		//image had neither aria-label or alt, add an empty alt
+		else{
+			
+			$(this).attr('alt', '');
+		}
+		
+		// if image has aria-hidden, remove it.
+		if ($(this).attr('aria-hidden') == 'true'){
+			$(this).removeAttr('aria-hidden');
+		}
+		
 	});
 	
-	//Matching Question accessibility fixes
-	//-----------------------------------------------------------------------------
+	// ----------------
+	// 
+	// ----------------	
+
+
 	
-	//Multiple Choice Question accessibility fixes
-	//-----------------------------------------------------------------------------
+	if($('#adapt').attr('data-location') == 'course'){
+	// ----------------
+	// MENU FIXES
+	// ----------------
+
+
+
+		
+	// ----------------
+	// 
+	// ----------------	
+	}
+
+
+
+	}else {
+	// ------------------
+	// COURSE PAGE FIXES
+	// ------------------
+
+		// multiple choice label fix
+		//-----------------------------------------------------------------------------
+		let multiChoiceComponents = $(".mcq-component");
+		multiChoiceComponents.each(function(i){
+			let label = $(this).attr("data-adapt-id") + "qlabel";
+			$(".mcq-body-inner > p").attr("id", label);
+			$(".mcq-widget").attr("aria-labelledby", label);
+		});
+
+		//Matching questions fix
+		//-----------------------------------------------------------------------------
+		$(".matching-select-container").each(function(k){
+			var glabel = $(this).parents().find('.matching-component').attr("data-adapt-id")+'_qlabel_'+k;
+			$(this).find(".dropdown__inner").attr('id', glabel);
+			$(this).find("button").attr('aria-labelledby', glabel);
+		});
+		
+		//Accordion component accessibility fixes
+		//-----------------------------------------------------------------------------
+		$(".accordion-component").each(function(){
+			var parentID = $(this).attr("data-adapt-id");
+			$(".accordion-item-title").each(function(i){
+				var blockid = "accord-" + i + "-" + parentID;
+				$(this).attr("aria-controls", blockid);
+				$(this).next().attr('id', blockid);
+			});
+		});
+
+		//remove tooltips from buttons
+		//-----------------------------------------------------------------------------
+		$("button").removeAttr("tooltip");
+		
+		
+	// ----------------
+	// 
+	// ----------------	
+	}
 	
-	//Graphical Multiple Choice Question accessibility fixes
-	//-----------------------------------------------------------------------------
 }
 
 
 
-//utility functions
-//=============================================================
+
+
+// -------------------------------------------------------------------------
+//
+//		Utility functions
+//
+// -------------------------------------------------------------------------
 
 //sort a multidimensional array
 //-----------------------------------------------------------------------------
@@ -127,3 +201,13 @@ function sortmulti(n, comparatorFunction, reverse) {
         }
     }
 }
+
+//Check if object has given attribute
+Object.prototype.hasAttr = function(attr) {
+    if(this.attr) {
+        var _attr = this.attr(attr);
+    } else {
+        var _attr = this.getAttribute(attr);
+    }
+    return (typeof _attr !== "undefined" && _attr !== false && _attr !== null);      
+};
